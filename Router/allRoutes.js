cdcdf8fc9be17664780a4198cd210985ec6ffcd5
samyra89 <myra89projects@gmail.com>
2023-12-31 17:11:59 +0100

    gestion de l'affichage des réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,7 +8,8 @@ export const allRoutes = [
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),];
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
