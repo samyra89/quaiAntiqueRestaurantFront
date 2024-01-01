3e5c3b43bb4d168e23c34a18d50390665b9d4dbc
samyra89 <myra89projects@gmail.com>
2024-01-01 16:35:57 +0100

    Add routage with role for security

--- Router/Route.js
+++ Router/Route.js
@@ -1,9 +1,18 @@
 // Route.js
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
-      this.url = url;
-      this.title = title;
-      this.pathHtml = pathHtml;
-      this.pathJS = pathJS;
-    }
+  constructor(url, title, pathHtml, authorize, pathJS = "") {
+    this.url = url;
+    this.title = title;
+    this.pathHtml = pathHtml;
+    this.pathJS = pathJS;
+    this.authorize = authorize;
+  }
 }
+/*
+[] -> tout le monde peut y accéder
+['disconnected'] -> Réserver utilisateur déconnecté
+['client'] -> Réserver utilisateur avec rôle client
+['admin'] -> Réserver utilisateur avec rôle admin
+['admin','client'] -> Réserver utilisateur avec rôle client ou admin
+
+*/
